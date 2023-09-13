694e7fb73e6e40f715cd859a5e76dfa5cd677a35
EmmanuelJunuior <emmanuelhooks@gmail.com>
2023-09-13 14:33:01 -0700

    0x03-debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative - prints if integer ios positive
+ *
+ *Return:
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
