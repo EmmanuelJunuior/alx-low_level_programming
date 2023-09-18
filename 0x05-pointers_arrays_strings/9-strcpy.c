a440d3a2b0335bed32b318e9f610020963df7564
EmmanuelJunuior <emmanuelhooks@gmail.com>
2023-09-18 12:29:42 -0700

    betty fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * char *_strcpy function that copies the string pointed to by src
+ * _strcpy - function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
@@ -10,7 +10,7 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
