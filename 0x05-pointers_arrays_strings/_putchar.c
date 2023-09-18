0493c968f81e2545dada3dc49176fe3397904594
EmmanuelJunuior <emmanuelhooks@gmail.com>
2023-09-18 12:00:19 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+*_putchar - writes the character c to stout
+*
+*Return: on (Success) 1
+*on error, -1 is returned, error set appropriately
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
